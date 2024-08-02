8135bf0522b1ac532370e4dbe2f1b1f8b41b0f84
0xChijioke <chijokeogbuanya@gmail.com>
2024-08-02 20:20:12 +0100

    card frame

---
+++ packages/nextjs/components/mecha/gallery/Frame.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+const Frame = (props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>) => (
+  <svg
+    height="auto"
+    viewBox="0 0 2040 2584"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M1942.51 110.143H100.078V2481.37H1942.51z"
+      stroke="#00CCC0"
+      strokeWidth={49.479}
+      strokeMiterlimit={1.5}
+      strokeLinecap="square"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M2015.14 210.08H24.86v2188.71h1990.28z"
+      stroke="#00CCC0"
+      strokeWidth={49.663}
+      strokeMiterlimit={1.5}
+      strokeLinecap="square"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M1865.17 25.213H177.414V2558.8H1865.17z"
+      stroke="#00CCC0"
+      strokeWidth={48.697}
+      strokeMiterlimit={1.5}
+      strokeLinecap="square"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default Frame;
