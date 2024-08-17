fa1ee79e27aa319fc7a72c7b8c5fc39162166b13
0xChijioke <chijokeogbuanya@gmail.com>
2024-08-17 19:49:55 +0100

    card details changes

--- packages/nextjs/components/mecha/gallery/CardDetails.tsx
+++ packages/nextjs/components/mecha/gallery/CardDetails.tsx
@@ -36,12 +36,12 @@ const CardDetails: FC<CardDetailsProps> = props => {
     window.open(url, "_blank");
   };
   return (
-    <div className="flex justify-center flex-col lg:pt-[4%] h-fit my-[15%] lg:my-0 lg:flex-row items-center w-full lg:max-h-screen">
-      <div className="pt-10 lg:pt-20 px-10 items-center">
+    <div className="flex justify-center w-full flex-col lg:pt-[4%] h-fit my-[15%] lg:my-0 lg:flex-row items-center lg:max-h-screen">
+      <div className="pt-10 lg:pt-8 px-10 items-center">
         <h1 className="text-2xl font-bold mb-4">
           {props.tokenId.slice(0, 4)}...{props.tokenId.slice(-4)}
         </h1>
-        <div className="flex w-full h-full lg:w-[60%] lg:h-[50%]">
+        <div className="flex w-full h-full lg:w-[60%]">
           <Image
             alt={`NFT ${props.tokenId}`}
             src={props.tokenMetadata.image}
@@ -53,9 +53,9 @@ const CardDetails: FC<CardDetailsProps> = props => {
           />
         </div>
       </div>
-      <div className="flex flex-col gap-y-4 mb-8 lg:mt-20 p-4 lg:p-8 text-xl space-y-2">
+      <div className="flex flex-col gap-y-4 text-left mb-8 w-1/2 lg:mt-20 p-4 lg:p-8 text-xl space-y-2">
         <p className="mb-2">{props.tokenMetadata.name}</p>
-        <p className="mb-2 lg:max-w-[40%]">{props.tokenMetadata.description}</p>
+        <p className="mb-2 lg:m">{props.tokenMetadata.description}</p>
         <p className="mb-2">{props.tokenMetadata.attributes[0].value} card</p>
         <div className="mb-2 flex gap-x-2 whitespace-nowrap">
           Owner <Address address={props.tokenMetadata.owner} />
